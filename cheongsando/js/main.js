88c311ecf9f53a86ce4d8c8fd14c806c3e13d3c0
ssj2540 <ssj2540@gmail.com>
2025-09-10 18:01:21 +0900

    250910 swiper

--- cheongsando/js/main.js
+++ cheongsando/js/main.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
         마우스를 올린 li에만 
         on 클래스 추가
     */
+
    $('.tour .list ul li').on('mouseenter', function(){
         //console.log('오버함')
         /* 
@@ -15,8 +16,32 @@ $(document).ready(function(){
         $('.tour .list ul li').removeClass('on')
         $(this).addClass('on')
    })
+
    $('footer .right_area .family_site button.open').on('click', function(){
-        console.log('클릭함')
+        // console.log('여는 버튼 클릭')
         $('footer .right_area .family_site').addClass('open')
    })
-})
+
+   $('footer .right_area .family_site button.close').on('click', function(){
+        // console.log('닫는 버튼 클릭')
+        $('footer .right_area .family_site').removeClass('open')
+   })
+
+   /* 
+        footer .right_area .top 클릭하면
+        브라우저가 상단으로 스크롤
+    */
+   $('footer .right_area .top').on('click', function(){
+        //console.log('누름')
+        /*
+            let scrolling = $(window).scrollTop()
+            console.log(scrolling)
+            $(window).scrollTop(0)
+        */
+        $('html, body').animate({
+            scrollTop: 0
+        }, 500)
+
+   })
+   
+})//$(document).ready
