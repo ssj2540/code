5dfac915058f8e0a23e20133a0923c340a34c092
ssj2540 <ssj2540@gmail.com>
2025-09-17 12:06:02 +0900

    모바일 테스트

---
+++ sdream/js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    let win_w = $(window).width()
+    let win_h = $(window).height()
+    console.log(win_w, win_h)
+    $('.wh').text('넓이는' + win_w + ' 높이는' + win_h)
+})
