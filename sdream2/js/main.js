6c406467699079a665f171bc5e755a3c751cbbe2
ssj2540 <ssj2540@gmail.com>
2025-09-21 02:59:45 +0900

    250921 집연습

---
+++ sdream2/js/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    console.log('연결')
+
+    const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
+
+        // autoplay: {  /* 팝업 자동 실행 */
+        //     delay: 1000,
+        //     disableOnInteraction: true,
+        // },
+
+        //effect: "fade", /* fade 효과 */
+
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+
+        navigation: {  /* 이전, 다음 버튼 */
+            nextEl: '.visual .btn_wrap .btn_next',  /* 다음 버튼의 클래스명 */
+            prevEl: '.visual .btn_wrap .btn_prev',
+        },
+
+    });
+
+    $('.visual .btn_wrap .btn_stop').on('click', function(){
+        console.log('멈춤')
+        visual_swiper.autoplay.stop();  /* 일시정지 기능 */
+    })
+    $('.visual .btn_wrap .btn_play').on('click', function () {
+        console.log('시작')
+        visual_swiper.autoplay.start();  /* 재생 기능 */
+    })
+    
+})//reday
