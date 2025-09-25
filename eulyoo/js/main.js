798debb24a74aba18c14f7aa470e9e77cc905011
ssj2540 <ssj2540@gmail.com>
2025-09-25 18:17:34 +0900

    250925 을유문화사

--- eulyoo/js/main.js
+++ eulyoo/js/main.js
@@ -48,6 +48,7 @@ $(document).ready(function(){
         2. 처음에 로딩했을 때도 체크
         ---> 동일한 체크를 두 번 실행 (스크롤 되어있는 상태에서 F5) --> 함수로 처리
     */
+
     let scrolling
     function scroll_chk(){ //함수를 정의한다는 의미
         scrolling = $(window).scrollTop()
@@ -64,4 +65,24 @@ $(document).ready(function(){
     $(window).scroll(function(){
         scroll_chk()
     })
+
+    //book swiper
+    const book_swiper = new Swiper('.book .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            769: {    /* 769px 이상일때 적용 */
+                slidesPerView: 'auto',    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                spaceBetween: 24,
+            },
+        },
+        //centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+        // loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+        
+        navigation: {
+            nextEl: '.book .btn_next',
+            prevEl: '.book .btn_prev',
+        },
+    });//swiper 종료
+
 })//ready
