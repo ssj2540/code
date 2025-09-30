aa90d70c01d26ce09e27ba2c3bdd8097573cc9fc
ssj2540 <ssj2540@gmail.com>
2025-09-30 21:59:52 +0900

    250930 을유문화사 연습

---
+++ practice/js/js_eulyoo/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    // console.log('연결')
+    const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
+
+        autoplay: {  /* 팝업 자동 실행 */
+            delay: 3000,
+            disableOnInteraction: true,
+        },
+
+        effect: "fade", /* fade 효과 */
+
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+
+        pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
+            el: '.visual .paging', /* 해당 요소의 class명 */
+            clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+        },
+
+
+        navigation: {  /* 이전, 다음 버튼 */
+            nextEl: '.visual .btn_next',  /* 다음 버튼의 클래스명 */
+            prevEl: '.visual .btn_prev',
+        },
+    })//swiper
+})//ready
