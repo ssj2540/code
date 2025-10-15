c72d937a71e6c446bfcb378110e28ca4d3c94f52
ssj2540 <ssj2540@gmail.com>
2025-10-15 18:01:54 +0900

    251015 유기동물보호센터

---
+++ animal/js/main.js
@@ -0,0 +1,55 @@
+$(document).ready(function (){
+
+/******************* 시작 ::visual swiper *******************/
+let visual_time = 5000
+const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
+
+	autoplay: {  /* 팝업 자동 실행 */
+		delay: visual_time,
+		disableOnInteraction: true,
+	},
+
+	//effect: "fade", /* fade 효과 */
+
+	loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+});
+
+$('.visual .btn_wrap .stop').on('click', function(){
+    visual_swiper.autoplay.stop();  /* 일시정지 기능 */
+    $(this).hide()
+    $('.visual .btn_wrap .play').css('display', 'flex')
+    $('.visual .btn_wrap .paging .bar span').stop() //animate 종료
+})
+$('.visual .btn_wrap .play').on('click', function(){
+    visual_swiper.autoplay.start();  /* 재생 기능 */
+    $(this).hide()
+    $('.visual .btn_wrap .stop').css('display', 'flex')
+    updateCurrent()
+})
+
+// 전체 슬라이드 개수 (loop 상태에서도 실제 슬라이드 개수만)
+const totalSlides = $('.visual .swiper .swiper-slide').not('.swiper-slide-duplicate').length;
+$('.visual .paging .total').text(totalSlides); // 총 개수 표시
+
+// 현재 슬라이드 번호 표시 함수
+function updateCurrent() {
+    let realIndex = visual_swiper.realIndex + 1; // 실제 인덱스 (0부터 시작하므로 +1)
+    $('.visual .paging .current').text(realIndex);
+    //슬라이드가 교체되면 넓이를 0으로 초기화
+    $('.visual .btn_wrap .paging .bar span').stop() //animate 종료
+    $('.visual .btn_wrap .paging .bar span').width(0)
+    $('.visual .btn_wrap .paging .bar span').animate({
+        width : '100%'
+    }, visual_time)
+}
+
+// 처음 로드 시 한번 실행
+updateCurrent();
+
+// 슬라이드 변경될 때마다 실행
+visual_swiper.on('slideChange', function () {
+    updateCurrent();
+});
+/******************* 끝 ::visual swiper *******************/
+
+}) //ready
