4757d0c517eaeb1089f3647c129a4e277f7bf049
ssj2540 <ssj2540@gmail.com>
2025-10-16 18:02:26 +0900

    251016 유기동물보호센터

--- animal/js/main.js
+++ animal/js/main.js
@@ -1,6 +1,29 @@
 $(document).ready(function (){
 
-/******************* 시작 ::visual swiper *******************/
+/******************* 시작 :: 현재 PC버전인지 mobile인지 체크 (메뉴상태) *******************/
+    
+let mobile_size = 1024
+let window_w
+let device_status // pc, mobile
+
+function device_chk(){ //함수를 정의(선언)한다
+    window_w = $(window).width()
+    if(window_w > mobile_size){
+        device_status = 'pc'
+    }else{
+        device_status = 'mobile'
+    }
+    console.log(device_status)
+}
+
+device_chk() //html의 로딩이 완료된 이후 단 1번 실행
+$(window).resize(function(){ //브라우저가 리사이즈 될때마다 실행
+    device_chk()
+})
+
+/******************* 끝 :: 현재 PC버전인지 mobile인지 체크 (메뉴상태) *******************/
+
+/******************* 시작 :: visual swiper *******************/
 let visual_time = 5000
 const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
 
@@ -50,6 +73,36 @@ updateCurrent();
 visual_swiper.on('slideChange', function () {
     updateCurrent();
 });
-/******************* 끝 ::visual swiper *******************/
+/******************* 끝 :: visual swiper *******************/
+
+/******************* 시작 :: PC버전 메뉴 오버 ******************
+ * 메뉴에 마우스 오버 했을 때
+ * header에 menu_pc 클래스 추가
+ * 마우스를 오버한 메뉴의 1차 메뉴 li에 over 클래스 추가  (header .gnb .gnb_wrap ul.depth1 > li)
+ * -> 오버한 li에만 over클래스 줌
+ * --> 모든 li에서 over를 빼고 오버한 li에만 over클래스 줌
+ * 색상의 영역 내부에선 오버 유저 / 밖에 나갈 때 오버 삭제
+ */
+
+$('header .gnb .gnb_wrap ul.depth1 > li').on('mouseenter focusin', function(){
+    if(device_status == 'pc'){ //pc일때만 동작
+    // console.log('메뉴오버')
+    $('header').addClass('menu_pc')
+    $('header .gnb .gnb_wrap ul.depth1 > li').removeClass('over')
+    $(this).addClass('over')
+    }
+})
+$('header .gnb .gnb_wrap ul.depth1 > li').on('mouseleave', function(){
+    // console.log('메뉴나감')
+    $(this).removeClass('over')
+})
+$('header').on('mouseleave', function(){
+    $(this).removeClass('menu_pc')
+})
+$('header .util .search .sch_open').on('focusin', function(){
+    $('header .gnb .gnb_wrap ul.depth1 > li').removeClass('over')
+})
+
+/******************* 시작 :: PC버전 메뉴 오버 *******************/
 
 }) //ready
