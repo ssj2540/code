45cc500aaf4859ad643c44e51a68bf45e7a4d917
ssj2540 <ssj2540@gmail.com>
2025-11-10 01:40:52 +0900

    251108 생명의숲 완성

--- forest/js/main.js
+++ forest/js/main.js
@@ -40,7 +40,7 @@ $(document).ready(function(){
 		},
 
 		responsiveWidth: 769,  /* fullpage를 적용시키지 않을 모바일의 넓이*/
-        responsiveHeight: 500   /* fullpage를 적용시키지 않을 모바일의 높이*/
+        responsiveHeight: 600   /* fullpage를 적용시키지 않을 모바일의 높이*/
 	}); //fullpage
 
 	let visual_name = ['서울마이트리', '2025 생명의숲 후원의달', '고목나무 이야기', '후원자 인터뷰']
@@ -63,7 +63,7 @@ $(document).ready(function(){
 				return '<li class="'+ className +'"><span>'+ visual_name[index] +'</span></li>';
 			},
 		},
-	});//swiper
+	});//visual_swiper
 
 	/*******************************
 	 * 퀵메뉴 열고 닫기
@@ -82,5 +82,17 @@ $(document).ready(function(){
 		$('aside.quick .quick_wrap').slideUp()
 	})
 
+	const news_swiper = new Swiper('.news .swiper', { /* 팝업을 감싼는 요소의 class명 */
+		slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+		spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+		breakpoints: {
+			1025: {    /* 1025px 이상일때 적용 */
+				slidesPerView: 4,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+				spaceBetween: 24,
+			},
+		},
+		//centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+		// loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+	});//news_swiper
 	
 })//ready
